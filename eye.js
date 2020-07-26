4eb09c1d3b98e4b7ec045413834d7fd589276679
yatrik-sopho1 <yatrik12@gmail.com>
2020-07-26 11:25:27 +0530

    Update eye.js

--- eye.js
+++ eye.js
@@ -24,8 +24,8 @@ display() {
 
     beginShape();
     vertex(-this.s,0);
-    bezierVertex(-30,-40,30,-40,this.s,0);
-    bezierVertex(30,40,-30,40,-this.s,0)
+    bezierVertex(-this.s/2,-this.s/1.5,this.s/2,-this.s/1.5,this.s,0);
+    bezierVertex(this.s/2,this.s/1.5,-this.s/2,this.s/1.5,-this.s,0)
     endShape();
   //  ellipse(0, 0, this.s,this.s/2);
     rotate(this.angle);
@@ -34,10 +34,10 @@ display() {
 
 
     fill(61,158,237);
-    ellipse(this.s/6, 0, this.s/1);
+    ellipse(this.s/8, 0, this.s/1);
 
 		fill(0);
-    ellipse(this.s/4.5, 0, this.s/1.6);
+    ellipse(this.s/5, 0, this.s/1.6);
 
     fill(255);
     ellipse(this.s/4.5, 0, this.s/15);
